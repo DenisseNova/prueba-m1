e4487c0e5adcaf115559698a226a29555b0e5e19
Denisse Nova <denisse.nova.v@gmail.com>
2021-07-21 20:48:09 -0400

    smooth scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,14 @@ $(document).ready(function(){
   $(".card .card-body .card-title").click(function(){
     $(this).siblings(".card-text").toggle(); //sibling llama al hermano
   });
-  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="tooltip"]').tooltip();
+
+
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+  });
 });
